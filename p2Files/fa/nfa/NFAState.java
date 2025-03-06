be3416157bd0034a67fdb384781a2e3e1cb2d5aa
Ashley Day <ashleyday672@u.boisestate.edu>
2025-03-06 11:54:34 -0700

    basic set up of classes TODO

--- p2Files/fa/nfa/NFAState.java
+++ p2Files/fa/nfa/NFAState.java
@@ -1,1 +1,25 @@
+package fa.nfa;
+
+import java.util.HashMap;
+
+import fa.State;
+/**
+ * This class represents a state in the NFA. It extends the abstract
+ * State class and manages transitions specific to this specific instance of
+ * state.
+ * 
+ * @author Ashley Day Brooke Matthews
+ */
+public class NFAState extends State {
+
+    /**
+     * Constructor for DFAState. Initializes the state with a name.
+     * 
+     * @param name The name of the state.
+     */
+    public NFAState(String name) {
+        super(name);
+    }
+    
+}
 
