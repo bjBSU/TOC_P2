2e9b8fa9ff25b6da3dd646520a080902a042c96c
Ashley Day <ashleyday672@u.boisestate.edu>
2025-03-06 13:46:40 -0700

    Constructor set up of State

--- p2Files/fa/nfa/NFAState.java
+++ p2Files/fa/nfa/NFAState.java
@@ -1,8 +1,9 @@
 package fa.nfa;
 
 import java.util.HashMap;
-
+import java.util.HashSet;
 import fa.State;
+
 /**
  * This class represents a state in the NFA. It extends the abstract
  * State class and manages transitions specific to this specific instance of
@@ -12,14 +13,25 @@ import fa.State;
  */
 public class NFAState extends State {
 
+    // Map for transitions: symbol -> set of states reachable by that symbol
+    private HashMap<Character, HashSet<NFAState>> transitions;
+    
+    // Flags to mark final and start states
+    private boolean isFinal;
+    private boolean isStart;
+
     /**
-     * Constructor for DFAState. Initializes the state with a name.
-     * 
+     * Constructor for NFAState. Initializes the state with a name.
      * @param name The name of the state.
      */
     public NFAState(String name) {
         super(name);
+        transitions = new HashMap<>();
+        isFinal = false;
+        isStart = false;
     }
+
+    
     
 }
 
